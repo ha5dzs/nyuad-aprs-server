3884ad076fc203fe810d9bef2866b8b1bd3e5ec4
ha5dzs <ha5dzs@gmail.com>
2023-09-28 17:34:29 +0400

    python debug begins

--- opt/trackdirect/server/trackdirect/__init__.py
+++ opt/trackdirect/server/trackdirect/__init__.py
@@ -1,6 +1,12 @@
-__version__ = "1.0"
-__author__ = "Per Qvarforth"
+__version__ = "1.1"
+__author__ = "Per Qvarforth, Zoltan Derzsi"
 
-from TrackDirectDataCollector import *
-from TrackDirectWebsocketServer import *
-from TrackDirectConfig import *
+#from TrackDirectDataCollector import *
+#from TrackDirectWebsocketServer import *
+#from TrackDirectConfig import *
+
+
+# In this new Python installation, something weird is going on with how modules are being imported.
+import trackdirect.TrackDirectDataCollector
+import trackdirect.TrackDirectWebsocketServer
+import trackdirect.TrankDirectConfig
